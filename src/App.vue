51ed4e380ce920b3d3b528f0c24e4f037c3ce658
pony-a <ponya209@gmail.com>
2024-06-03 14:03:05 +0900

    最初のコミット 地図を表示

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MapLibreMap from './components/MapLibreMap.vue'
+</script>
+
+<template>
+  <div>
+    <MapLibreMap />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MapLibreMap.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import 'maplibre-gl/dist/maplibre-gl.css'
+import { Map } from 'maplibre-gl'
+import { onMounted } from 'vue'
+onMounted(() => {
+    new Map({
+        container: 'map',
+        style: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/pale.json',
+        center: [140.084556, 36.104611],
+        zoom: 11,
+    })
+})
+
+</script>
+<template>
+    <div id="map"></div>
+</template>
+<style scoped>
+#map {
+    height: 100vh;
+}
+</style>
