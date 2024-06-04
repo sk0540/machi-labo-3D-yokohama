0301950f983c504d4ccc1949abf5947ccfdc6679
pony-a <ponya209@gmail.com>
2024-06-04 17:24:38 +0900

    ベクトルタイルに対応&陰影起伏追加

--- src/components/MapLibreMap.vue
+++ src/components/MapLibreMap.vue
@@ -6,38 +6,55 @@ import { useGsiTerrainSource } from 'maplibre-gl-gsi-terrain';
 onMounted(() => {
     const gsiTerrainSource = useGsiTerrainSource(maplibreGl.addProtocol);
 
-    new Map({
+    const map = new Map({
         container: 'map',
-        maxPitch: 67,
-        style: {
-            version: 8,
-            sources: {
-                gsipale: {
-                    type: 'raster',
-                    tiles: [
-                        'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
-                    ],
-                    attribution: '地理院タイル',
-                },
-                terrain: gsiTerrainSource,
-            },
-            layers: [
+        style: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/pale.json',
+        /*
                 {
-                    id: 'gsipale',
-                    type: 'raster',
-                    source: 'gsipale',
-                },
-            ],
-            terrain: {
-                source: 'terrain',
-                exaggeration: 1.2,
-            },
-        },
+                    version: 8,
+                    sources: {
+                        gsipale: {
+                            type: 'raster',
+                            tiles: [
+                                'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
+                            ],
+                            attribution: '国土地理院',
+                        },
+                        gsidem: gsiTerrainSource,
+                    },
+                    layers: [
+                        {
+                            id: 'gsipale',
+                            type: 'raster',
+                            source: 'gsipale',
+                        },
+                    ],
+                    terrain: {
+                        source: 'gsidem',
+                        exaggeration: 1.2,
+                    },
+                },*/
         center: [139.767125, 35.681236],
         zoom: 11,
+        maxPitch: 68,
     });
-
-})
+    map.on('load', () => {
+        map.addSource('terrain', gsiTerrainSource);
+        map.addLayer({
+            id: 'hills',
+            type: 'hillshade',
+            source: 'terrain',
+        },
+            'gsibv-vectortile-layer-1539'//国土地理院タイルの最背面レイヤの更に背面に追加
+        );
+        map.addLayer({
+            id: 'background',
+            type: 'background',
+            paint: { 'background-color': '#fff' }
+        }, 'hills');//タイルの境目に標高ノイズを発生させないための白背景
+        map.setTerrain({ source: 'terrain' },);
+    });
+});
 
 </script>
 <template>
