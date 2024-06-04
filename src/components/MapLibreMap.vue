ba1ab3fb33da994e4d6fd38cffcc9a04b22df42a
pony-a <ponya209@gmail.com>
2024-06-04 09:32:28 +0900

    標高モデルを読込 地形3D表示

--- src/components/MapLibreMap.vue
+++ src/components/MapLibreMap.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
 import 'maplibre-gl/dist/maplibre-gl.css'
-import { Map } from 'maplibre-gl'
+import maplibreGl, { Map } from 'maplibre-gl'
 import { onMounted } from 'vue'
+import { useGsiTerrainSource } from 'maplibre-gl-gsi-terrain';
 onMounted(() => {
+    const gsiTerrainSource = useGsiTerrainSource(maplibreGl.addProtocol);
+
     new Map({
         container: 'map',
-        style: 'https://gsi-cyberjapan.github.io/gsivectortile-mapbox-gl-js/pale.json',
-        center: [140.084556, 36.104611],
+        maxPitch: 67,
+        style: {
+            version: 8,
+            sources: {
+                gsipale: {
+                    type: 'raster',
+                    tiles: [
+                        'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
+                    ],
+                    attribution: '地理院タイル',
+                },
+                terrain: gsiTerrainSource,
+            },
+            layers: [
+                {
+                    id: 'gsipale',
+                    type: 'raster',
+                    source: 'gsipale',
+                },
+            ],
+            terrain: {
+                source: 'terrain',
+                exaggeration: 1.2,
+            },
+        },
+        center: [139.767125, 35.681236],
         zoom: 11,
-    })
+    });
+
 })
 
 </script>
